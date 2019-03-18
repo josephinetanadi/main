14570b871a2c946e335cd7a422c71ac19ead4b99
gautamrajulu <gx1996@gmail.com>
2019-03-18 12:38:36 +0800

    fix checkstyle

--- src/test/java/seedu/project/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/project/logic/commands/AddCommandTest.java
@@ -24,8 +24,8 @@ import seedu.project.model.ReadOnlyProjectList;
 import seedu.project.model.ReadOnlyUserPrefs;
 import seedu.project.model.project.Project;
 import seedu.project.model.project.ReadOnlyProject;
-import seedu.project.model.task.Task;
 import seedu.project.model.tag.Tag;
+import seedu.project.model.task.Task;
 import seedu.project.testutil.TaskBuilder;
 
 public class AddCommandTest {
