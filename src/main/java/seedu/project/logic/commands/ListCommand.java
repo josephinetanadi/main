ece72068583b65b302a9fe4e1b65f890f1d9abdd
gautamrajulu <gx1996@gmail.com>
2019-03-22 15:47:29 +0800

    ListCommand: enable listing of task and project

--- src/main/java/seedu/project/logic/commands/ListCommand.java
+++ src/main/java/seedu/project/logic/commands/ListCommand.java
@@ -1,9 +1,11 @@
 package seedu.project.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.project.model.Model.PREDICATE_SHOW_ALL_PROJECTS;
 import static seedu.project.model.Model.PREDICATE_SHOW_ALL_TASKS;
 
 import seedu.project.logic.CommandHistory;
+import seedu.project.logic.LogicManager;
 import seedu.project.model.Model;
 
 /**
@@ -13,12 +15,19 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
     public static final String COMMAND_ALIAS = "l";
-    public static final String MESSAGE_SUCCESS = "Listed all tasks";
+
+    public static final String MESSAGE_SUCCESS_PROJECT = "Listed all projects";
+    public static final String MESSAGE_SUCCESS_TASK = "Listed all tasks";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (!LogicManager.getState()) {
+            model.updateFilteredProjectList(PREDICATE_SHOW_ALL_PROJECTS);
+            return new CommandResult(MESSAGE_SUCCESS_PROJECT);
+        } else {
+            model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+            return new CommandResult(MESSAGE_SUCCESS_TASK);
+        }
     }
 }
