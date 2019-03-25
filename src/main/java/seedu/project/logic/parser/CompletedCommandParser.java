f9d2d50ac5488a8e88fb4eeec59c175eb0355c74
Rachel Lim Fang Yu <rachellim10@gmail.com>
2019-03-25 21:57:08 +0800

    add CompletedCommand as part of analyse feature

---
+++ src/main/java/seedu/project/logic/parser/CompletedCommandParser.java
@@ -0,0 +1,43 @@
+package seedu.project.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.project.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.InputMismatchException;
+
+import seedu.project.commons.core.index.Index;
+import seedu.project.logic.commands.CompletedCommand;
+import seedu.project.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new CompletedCommand object
+ */
+public class CompletedCommandParser implements Parser<CompletedCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the CompletedCommand
+     * and returns an CompletedCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public CompletedCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap;
+        try {
+            argMultimap = ArgumentTokenizer.tokenize(args);
+        }
+        catch (InputMismatchException ime) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    CompletedCommand.MESSAGE_USAGE), ime);
+        }
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        }
+        catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    CompletedCommand.MESSAGE_USAGE), pe);
+        }
+
+        return new CompletedCommand(index);
+    }
+}
--- src/main/java/seedu/project/logic/parser/ProjectParser.java
+++ src/main/java/seedu/project/logic/parser/ProjectParser.java
@@ -10,6 +10,7 @@ import seedu.project.logic.commands.AddCommand;
 import seedu.project.logic.commands.ClearCommand;
 import seedu.project.logic.commands.Command;
 import seedu.project.logic.commands.CompareCommand;
+import seedu.project.logic.commands.CompletedCommand;
 import seedu.project.logic.commands.DeleteCommand;
 import seedu.project.logic.commands.EditCommand;
 import seedu.project.logic.commands.ExitCommand;
@@ -63,6 +64,12 @@ public class ProjectParser {
 
         case CompareCommand.COMMAND_ALIAS:
             return new CompareCommandParser().parse(arguments);
+
+        case CompletedCommand.COMMAND_WORD:
+            return new CompletedCommandParser().parse(arguments);
+
+        case CompletedCommand.COMMAND_ALIAS:
+            return new CompletedCommandParser().parse(arguments);
 
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
